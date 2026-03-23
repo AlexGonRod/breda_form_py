6da2e6012be17c8ed8f84cccc79372284f0b582d
Alex Gonzalez <alexgonrod83@gmail.com>
2026-03-23 21:53:18 +0100

    feat: change messages to catalan

--- formulari_app/lib/tests/test_error_handling.py
+++ formulari_app/lib/tests/test_error_handling.py
@@ -4,25 +4,25 @@ from formulari_app.lib.error_handling import PermissionDenied, WorksheetNotFound
 class TestErrors():
     def test_permission_denied(self):
         with pytest.raises(PermissionDenied) as exc_info:
-            raise PermissionDenied("Acceso no autorizado")
-        assert str(exc_info.value) == "Permiso denegado: Acceso no autorizado"
+            raise PermissionDenied("Accés no autoritzat")
+        assert str(exc_info.value) == "Permis denegat: Accés no autoritzat"
 
     def test_worksheetNotFound(self):
         with pytest.raises(WorksheetNotFound) as exc_info:
-            raise WorksheetNotFound("Formulario")
-        assert str(exc_info.value) == "Worksheet no encontrada: 'Formulario'"
+            raise WorksheetNotFound("Formulari")
+        assert str(exc_info.value) == "Worksheet no trobada: 'Formulari'"
 
     def test_SpreadsheetNotFound(self):
         with pytest.raises(SpreadsheetNotFound) as exec_info:
             raise SpreadsheetNotFound('1234')
-        assert str(exec_info.value) == "Spreadsheet no encontrada: '1234'"
+        assert str(exec_info.value) == "Spreadsheet no trobada: '1234'"
 
     def test_APIError(self):
         with pytest.raises(APIError) as exec_info:
-            raise APIError("Error al conectar con la API")
-        assert str(exec_info.value) == "Error de API: Error al conectar con la API"
+            raise APIError("Error en connectar amb l'API")
+        assert str(exec_info.value) == "Error de l'API: Error en connectar amb l'API"
 
     def test_DataAppendError(self):
         with pytest.raises(DataAppendError) as exec_info:
-            raise DataAppendError("Error al añadir datos a la hoja")
-        assert str(exec_info.value) == "Error al añadir datos: Error al añadir datos a la hoja"
+            raise DataAppendError("Error en afegir dades al full")
+        assert str(exec_info.value) == "Error al afegir dades: Error en afegir dades al full"
