488e2a753cdce73f6c2f65ecd77562c8fc1d9f0d
AlexGonRod <alexgonrod83@gmail.com>
2026-07-10 15:05:48 +0200

    fix: fixing text wrap in responsive for wrapper

--- formulari_app/pages/formulari/components/wrapper.py
+++ formulari_app/pages/formulari/components/wrapper.py
@@ -15,7 +15,7 @@ def wrapper(sheet_name: str) -> rx.Component:
                 rx.heading("Breda de l'Eixample", as_="h2", size="6", align="center", margin_bottom="16px"),
                 rx.cond(
                     sheet_name == "tast",
-                    rx.text("Si us plau, ompliu el formulari per participar en el Tast de vins.\nAquest any, el pagamanet es farà per transferència bancària.\nUna vegada ompliu el formulari, rebreu per missatge al móvil les dades corresponents.", white_space="pre"),
+                    rx.text("Si us plau, ompliu el formulari per participar en el Tast de vins.\nAquest any, el pagamanet es farà per transferència bancària.\nUna vegada ompliu el formulari, rebreu per missatge al móvil les dades corresponents.", white_space="pre-wrap", ),
                     rx.text("Si us plau, ompliu el formulari per participar en el Concurs de paelles."),
                 ),
                 rx.text("Qualsevol dubte, posseu-vos en contacte amb: ", rx.link("bredainfocat@gmail.com ", href="mailto:bredainfocat@gmail.com")),
@@ -26,5 +26,6 @@ def wrapper(sheet_name: str) -> rx.Component:
                 direction="column",
                 spacing="1",
                 width="100%",
+                text_wrap="auto"
                 # class_name="bg-white text-black",
             )
